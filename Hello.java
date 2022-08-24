3ecd31350d832b5dff1f46ce62823afb772311cd
AsishTamang <112000270+AsishTamang@users.noreply.github.com>
2022-08-24 11:28:00 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*************************************************
+* Java Program Name 
+**************************************************
+*Student Name (Asish Tamang)
+*08-24-21
+*Course Number and Section Number
+**************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Asish Tamang>");
+	System.out.println("<Java program Name>");
+	System.out.println("<Program/Project Desscription>");
+	System.out.println("<Date Created>");
+	System.out.println("<Course Number and Section Number>");
+
+	}
+}
